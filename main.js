e4260920ac6018a9e3179764fe3602b5308a30b2
Sofia Varich <kirisona@gmail.com>
2019-09-16 14:04:23 +0300

    problems with task 2

---
+++ main.js
@@ -0,0 +1,118 @@
+/**На основе массива map и массива users собрать новый массив объектов где в каждом объекте будут только те свойства которые перечислены в массиве map 
+ * На выходе мы должны получить массив вида: 
+
+    [ { _id: ..., name: ..., isActive: ..., balance: ... }, ... ];
+*/
+
+const map = ["_id", "name", "isActive", "balance"];
+const users = [
+      {
+    "_id": "5d220b10e8265cc978e2586b",
+    "isActive": true,
+    "balance": 2853.33,
+    "age": 20,
+    "name": "Buckner Osborne",
+    "gender": "male",
+    "company": "EMPIRICA",
+    "email": "bucknerosborne@empirica.com",
+    "phone": "+1 (850) 411-2997",
+    "registered": "2018-08-13T04:28:45 -03:00",
+    "nestedField": { total: 300 }
+  },
+  {
+    "_id": "5d220b10144ef972f6c2b332",
+    "isActive": true,
+    "balance": 1464.63,
+    "age": 38,
+    "name": "Rosalie Smith",
+    "gender": "female",
+    "company": "KATAKANA",
+    "email": "rosaliesmith@katakana.com",
+    "phone": "+1 (943) 463-2496",
+    "registered": "2016-12-09T05:15:34 -02:00",
+    "nestedField": { total: 400 }
+  },
+  {
+    "_id": "5d220b1083a0494655cdecf6",
+    "isActive": false,
+    "balance": 2823.39,
+    "age": 40,
+    "name": "Estrada Davenport",
+    "gender": "male",
+    "company": "EBIDCO",
+    "email": "estradadavenport@ebidco.com",
+    "phone": "+1 (890) 461-2088",
+    "registered": "2016-03-04T03:36:38 -02:00",
+  	"nestedField": { total: 200 }
+  }
+];
+
+let newArray = [];
+
+for (let i = 0; i < users.length; i++) {
+  let user = users[i];
+  let newUser = {};
+  for (let j = 0; j < map.length; j++) {
+    if (user.hasOwnProperty(map[j])) {
+      newUser[map[j]] = user[map[j]];
+    }
+  }
+  newArray.push(newUser);
+}
+console.log(newArray);
+
+
+/**2. Написать функцию сортировки которая принимает массив объектов который хотим сортировать, поле по которому хотим сортировать, и в каком порядке сортировать по возрастанию asc или по убыванию desc.
+
+Условия:
+
+- сортировка может быть как по числу так и по строке, т.е поле по которому мы хотим сортировать может содержать как строку так и число.
+- передаваемый массив не должен изменяться
+- нужно проверять что передан массив и поле по которому сортировать. */
+
+// function sortArray(array, key, order) {
+//   for (let i = 0; i < array.length; i++) {
+//     if (array[i].key > array[i + 1].key) {
+//       return order = 1;
+//     } else if (array[i].key < array[i + 1].key) {
+//       return order = -1;
+//     } else {
+//       return 0;
+//     }
+//   }
+// }
+
+// users.sort(sortArray);
+
+// function sortAsc(a, b, key) {
+//   if (a.key > b.key) {
+//     return 1;
+//   } else if (a.key < b.key) {
+//     return -1;
+//   } else {
+//     return 0;
+//   }
+// }
+
+// function sortDesc(a, b, key) {
+//   return sortAsc(b, a, key);
+// }
+
+// function sortArray(array, index, key) {
+//   if (sortOrder > 0) {
+//     users.sort(function (a, b) { return sortDesc(a, b, key); });
+//     sortOrder = -1;
+//   } else {
+//     users.sort(function (a, b) { return sortAsc(a, b); });
+//     sortOrder = 1;
+//   }
+
+// }
+
+function sortArray(array, key, order) {
+  for (let i = 0; i < array.length; i++) {
+    array[i].sort();
+  }
+}
+
+console.log(sortArray(newArray, 'balance'))
